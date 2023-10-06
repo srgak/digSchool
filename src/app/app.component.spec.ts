aaf71fc55cfcc6b90a185594e1dcbdae920a3ba4
Sergey Akimov <sergeyakimov@MacBook-Pro-Sergey.local>
2023-10-06 11:35:37 +0300

    написание тестов для компонента

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,48 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { AccessTokenService } from './services/storage/access-token/access-token.service';
+import { ModalService } from './services/modal/modal.service';
+import { RouterModule } from '@angular/router';
 
-// describe('AppComponent', () => {
-  
-// });
+describe('AppComponent', () => {
+  let component: AppComponent;
+  let fiixture: ComponentFixture<AppComponent>;
+  let accessTokenService;
+  let modalService;
+
+  beforeEach(() => {
+    accessTokenService = {
+      get prop(): string {
+        return 'test';
+      }
+    };
+    modalService = {
+      isOpen: false
+    };
+
+    TestBed.configureTestingModule({
+      declarations: [AppComponent],
+      imports: [
+        RouterModule
+      ],
+      providers: [
+        {
+          provide: AccessTokenService,
+          useValue: accessTokenService
+        },
+        {
+          provide: ModalService,
+          useValue: modalService
+        }
+      ]
+    });
+
+    fiixture = TestBed.createComponent(AppComponent)
+  });
+
+  it('Создать компонент', () => {
+    component = fiixture.componentInstance;
+    expect(component).toBeDefined();
+  });
+});
