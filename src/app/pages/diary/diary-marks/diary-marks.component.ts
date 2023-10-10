23e7e37bb8ccd6f7736b54fd818fbbd1026c6656
Sergey Akimov <sergeyakimov@MacBook-Pro-Sergey.local>
2023-10-10 17:19:42 +0300

    добавлен вывод оценок в виде таблицы

--- src/app/pages/diary/diary-marks/diary-marks.component.ts
+++ src/app/pages/diary/diary-marks/diary-marks.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
 import { ActivatedRoute } from '@angular/router';
 import { Observable, Subscription } from 'rxjs';
 import { MarkValue } from 'src/app/helpers/interfaces/marks';
@@ -12,12 +13,19 @@ import { MarksDataService } from 'src/app/services/marks/marks.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    MatTableModule
   ]
 })
 export class DiaryMarksComponent implements OnInit, OnDestroy {
   public currentMarks!: Observable<MarkValue[] | string>;
   private subs: Subscription = new Subscription();
+  public columnList: string[] = [
+    'date',
+    'value',
+    'type',
+    'description'
+  ];
   constructor(
     private activeRoute: ActivatedRoute,
     public marksData: MarksDataService
