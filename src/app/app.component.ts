3ba7783d429be6b5406586cfe545205d516de206
Sergey Akimov <sergeyakimov@MacBook-Pro-Sergey.local>
2023-11-13 11:01:20 +0300

    чистка кода

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,6 @@
-import { Component, Inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { ModalService } from './services/modal/modal.service';
 import { AccessTokenService } from './services/storage/access-token/access-token.service';
-import { BREADCRUMBS } from './helpers/tokens/breadcrumbs';
-import { Observable } from 'rxjs';
-import { BreadcrumbItem } from './helpers/interfaces/breadcrumbs';
-import { Router } from '@angular/router';
 import { BreadcrumbsService } from './services/breadcrumbs/breadcrumbs.service';
 
 @Component({
@@ -16,10 +12,6 @@ export class AppComponent {
   constructor(
     public accessToken: AccessTokenService,
     public modal: ModalService,
-    private router: Router,
     public breadcrumbsData: BreadcrumbsService
-    // @Inject(BREADCRUMBS) public breadcrumbs: Observable<BreadcrumbItem[]>
-  ) {
-    // this.router.events.subscribe(console.log);
-  }
+  ) {}
 }
